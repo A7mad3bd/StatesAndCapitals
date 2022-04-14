d736250d0f8fd7d4d67466033b2e33973deebabd
A7mad3bd <ahmadabd398@gmail.com>
2022-04-14 03:52:46 +0300

    done

---
+++ src/main/java/com/example/StatesAndCapitals/StatesAndCapitalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.StatesAndCapitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatesAndCapitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatesAndCapitalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StatesAndCapitals/StatesAndCapitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StatesAndCapitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatesAndCapitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
